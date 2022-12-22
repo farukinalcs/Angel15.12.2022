653965ff30c228b285c2b795aa07231fd1cd8bb3
farukinal\Meyer <farukinalcs@gmail.com>
2022-12-22 10:08:24 +0300

    talepler klasorü componentleri oluşturuldu

---
+++ src/app/_angel/profile/talepler/mobillokasyon/mobillokasyon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobillokasyon',
+  templateUrl: './mobillokasyon.component.html',
+  styleUrls: ['./mobillokasyon.component.scss']
+})
+export class MobillokasyonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/_angel/profile/talepler/taleplerim/taleplerim.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-taleplerim',
+  templateUrl: './taleplerim.component.html',
+  styleUrls: ['./taleplerim.component.scss']
+})
+export class TaleplerimComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
