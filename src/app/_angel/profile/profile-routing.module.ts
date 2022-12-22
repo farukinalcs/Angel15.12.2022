d7d237ec7cb04ccbe2ef5f8f23ed8061ca7b53a4
farukinal\Meyer <farukinalcs@gmail.com>
2022-12-22 10:16:26 +0300

    profil routing

--- src/app/_angel/profile/profile-routing.module.ts
+++ src/app/_angel/profile/profile-routing.module.ts
@@ -5,6 +5,12 @@ import { Routes,RouterModule } from '@angular/router';
 import { GecislerimComponent } from './islemler/gecislerim/gecislerim.component';
 import { IzinlerimComponent } from './islemler/izinlerim/izinlerim.component';
 import { SurelerimComponent } from './islemler/surelerim/surelerim.component';
+import { TalepedilenlerComponent } from './talepler/talepedilenler/talepedilenler.component';
+import { TaleplerimComponent } from './talepler/taleplerim/taleplerim.component';
+import { ZiyaretcitaleplerimComponent } from './talepler/ziyaretcitaleplerim/ziyaretcitaleplerim.component';
+import { MobillokasyonComponent } from './talepler/mobillokasyon/mobillokasyon.component';
+import { TasklistemComponent } from './talepler/tasklistem/tasklistem.component';
+import { TakimimComponent } from './talepler/takimim/takimim.component';
 
 const routes: Routes = [
   {
@@ -22,6 +28,30 @@ const routes: Routes = [
       {
         path: 'surelerim',
         component: SurelerimComponent,
+      },
+      {
+        path: 'talep_edilenler',
+        component: TalepedilenlerComponent,
+      },
+      {
+        path: 'taleplerim',
+        component: TaleplerimComponent,
+      },
+      {
+        path: 'ziyaretci_taleplerim',
+        component: ZiyaretcitaleplerimComponent,
+      },
+      {
+        path: 'mobil_lokasyon',
+        component: MobillokasyonComponent,
+      },
+      {
+        path: 'task_listem',
+        component: TasklistemComponent,
+      },
+      {
+        path: 'takimim',
+        component: TakimimComponent,
       },
       { path: '', redirectTo: 'gecislerim', pathMatch: 'full' },
       { path: '**', redirectTo: 'gecislerim', pathMatch: 'full' },
